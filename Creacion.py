5dbe049eaee3add44fa73fc6d53abdc0974aa9a3
Axel Peralta <76273740+Kurosios@users.noreply.github.com>
2023-11-05 05:02:55 -0300

    Update Creacion.py

--- Creacion.py
+++ Creacion.py
@@ -1,12 +1,11 @@
 import Nivel as lv
 import Habilidad as hb
 import Inventario as bag
-
+import Equipo as eq
 class Personaje :
     def __init__ (self):
         self.nivel = lv.Level()
         self.habilidad = hb.Habilidades()
         self.vida = self.habilidad.constitucion.vida
         self.inventario = bag.Bag()
-
-
+        self.equipo = eq.Equipo ()
